5cd79ae1f29736b8ceb50daf43db58bba0df2b3e
Stf Kolev <inkyzfx@gmail.com>
2019-05-21 10:26:37 +0300

    Update index.js
    Autospawning shards

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config/config.json');
 
-const manager = new ShardingManager('./amxxbot.js', {token: token });
+const manager = new ShardingManager('./amxxbot.js', { 
+    token: token,
+    autoSpawn: true
+});
 
 manager.spawn();
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
